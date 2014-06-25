2f41a8641ae39a5f0c8659a92d70e16786f136b1
Isaac Greenspan <ilg@2718.us>
2014-06-25 11:12:29 -0500

    Imported user code snippets

---
+++ define_weak_self.m
@@ -0,0 +1,10 @@
+// Define Weak Self
+// 
+//
+// IDECodeSnippetCompletionPrefix: vokWeakSelf
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: 83F921BC-126F-4751-9E91-40C5E3F631E6
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+typeof(self) __weak weakSelf = self;
