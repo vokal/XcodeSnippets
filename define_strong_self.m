e63e1ee6ba1d6b41ab0158cbbb8c45201f5dbc95
eckrag89 <erik.krag@vokalinteractive.com>
2015-09-22 10:52:12 -0500

    Updated strongSelf snippet per comments

--- define_strong_self.m
+++ define_strong_self.m
@@ -7,4 +7,4 @@
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
 // IDECodeSnippetVersion: 2
-__typeof__(self) strongSelf = weakSelf;
+typeof(weakSelf) strongSelf = weakSelf;
